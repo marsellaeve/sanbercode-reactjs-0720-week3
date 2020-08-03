cb2ab064784cf3643892c65ea0e7595067036988
marsellaeve <marsella.eve@gmail.com>
2020-08-03 14:38:02 +0700

    tugas11

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DaftarHargaBuah from './tugas11/tugas11.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <DaftarHargaBuah />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/tugas11/tugas11.js
@@ -0,0 +1,54 @@
+import React from 'react';
+let dataHargaBuah = [
+    {nama: "Semangka", harga: 10000, berat: 1000},
+    {nama: "Anggur", harga: 40000, berat: 500},
+    {nama: "Strawberry", harga: 30000, berat: 400},
+    {nama: "Jeruk", harga: 30000, berat: 1000},
+    {nama: "Mangga", harga: 30000, berat: 500}
+]
+class DaftarHargaBuah extends React.Component{
+    render(){
+        return(
+            <div>
+                <style>
+                    {`
+                    div{
+                        padding-left:2px;
+                        font-family:serif;
+                    }
+                    table{
+                        border: 1px solid black;
+                        width: 700px;
+                    }
+                    th {
+                        background-color: #aaaaaa;
+                    }
+                    td {
+                        background-color:#ff7f50;
+                    }
+                    h1{text-align:center;}`}
+                </style>
+                <h1>Tabel Harga Buah</h1>
+                <table>
+                    <tr>
+                        <th>Nama</th>
+                        <th>Harga</th>
+                        <th>Berat</th>
+                    </tr>
+            {
+                dataHargaBuah.map(el=>{
+                    return(
+                        <tr>
+                            <td>{el.nama}</td>
+                            <td>{el.harga}</td>
+                            <td>{el.berat/1000+' kg'}</td>
+                        </tr>
+                    )
+                })
+            }
+            </table>
+            </div>
+        )
+    }
+}
+export default DaftarHargaBuah;
